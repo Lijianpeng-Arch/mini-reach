6eaeee9da265a8eb73635efafd1a8f1e93a8936d
Lijianpeng-Arch <1928937943@qq.com>
2026-06-29 21:39:27 +0800

    feat: add Bilibili channel

--- mini_reach/channels/__init__.py
+++ mini_reach/channels/__init__.py
@@ -5,5 +5,6 @@ from mini_reach.channels.web import WebChannel
 from mini_reach.channels.github import GitHubChannel
 from mini_reach.channels.rss import RSSChannel
 from mini_reach.channels.youtube import YouTubeChannel
+from mini_reach.channels.bilibili import BilibiliChannel
 
-__all__ = ["Channel", "WebChannel", "GitHubChannel", "RSSChannel", "YouTubeChannel"]
+__all__ = ["Channel", "WebChannel", "GitHubChannel", "RSSChannel", "YouTubeChannel", "BilibiliChannel"]
