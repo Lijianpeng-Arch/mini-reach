b6180138c13c46a7aebc98407a73157f9425b97d
Lijianpeng-Arch <1928937943@qq.com>
2026-06-29 21:02:24 +0800

    feat: update channels init

--- mini_reach/channels/__init__.py
+++ mini_reach/channels/__init__.py
@@ -1,5 +1,9 @@
 """channels 模块 - 各个平台的统一接口"""
 
 from mini_reach.channels.base import Channel
+from mini_reach.channels.web import WebChannel
+from mini_reach.channels.github import GitHubChannel
+from mini_reach.channels.rss import RSSChannel
+from mini_reach.channels.youtube import YouTubeChannel
 
-__all__ = ["Channel"]
+__all__ = ["Channel", "WebChannel", "GitHubChannel", "RSSChannel", "YouTubeChannel"]
